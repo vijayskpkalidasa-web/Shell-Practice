a4881110d3b677335d99426b059dc7d59b3e766d
vijayskpkalidasa-web <vijayskp.kalidasa@gmail.com>
2026-05-01 07:01:27 +0530

    Delete 04.Arguments.sh

--- 04.Arguments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VAR1=$1
-VAR2=$2
-
-echo "$VAR1:are you suresh"
-echo "$VAR2: i am fine how are you doing"
-echo "$VAR1 : thanks for asking, $VAR2 what you are learning "
-echo "$VAR2 : i am learning devops"
