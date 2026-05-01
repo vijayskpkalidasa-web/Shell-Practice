61d1cc0f6f8f27b6aef49c27b9c02b4e8ea09c59
vijayskpkalidasa-web <vijayskp.kalidasa@gmail.com>
2026-05-01 07:01:02 +0530

    Delete 02.Conversation.sh

--- 02.Conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Ramesh :how are you suresh"
-echo "Suresh: i am fine how are you doing"
-echo "Ramesh : thanks for asking, Suresh what you are learning "
-echo "Suresh : i am learning devops"
