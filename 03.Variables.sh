64331bfd8182547ba13609fc196cb9d91c9e2df1
VijayKalidasa <VijayKalidasa>
2026-05-01 23:02:59 +0530

    new changes commited

---
+++ 03.Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VAR1=Suresh
+VAR2=Ramesh
+
+
+echo "$VAR1:How are you $VAR2"
+echo "$VAR2: I am doing good,thanks for asking"
+echo "$VAR1: What are you learning, $VAR2"
+echo "$VAR2: I am learning shell scripting"
