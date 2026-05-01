16236232b4d475f1f7d4d1dd11186f09c99a351a
vijayskpkalidasa-web <vijayskp.kalidasa@gmail.com>
2026-05-01 07:01:14 +0530

    Delete 03.Variables.sh

--- 03.Variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VAR1=Ramesh
-VAR2=Suresh
-
-echo "$VAR1:are you suresh"
-echo "$VAR2: i am fine how are you doing"
-echo "$VAR1 : thanks for asking, $VAR2 what you are learning "
-echo "$VAR2 : i am learning devops"
