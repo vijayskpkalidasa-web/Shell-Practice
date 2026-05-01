430fd674a07e1ad9e33c2ddd6ca169e01194d8a0
VijayKalidasa <VijayKalidasa>
2026-05-01 07:46:18 +0530

    file changes

---
+++ Arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Var1=$1
+Var2=$2
+
+echo "$Var1: How are you"
+echo "$Var2: I am doing good,thanks for asking"
+echo "$Var1: what are you learning $Var2"
+echo "$Var2: I am learning devops"
