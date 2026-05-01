8788600ac9672d48a48a311b28af678e30c98ee7
VijayKalidasa <VijayKalidasa>
2026-05-01 22:59:03 +0530

    new changes commited

---
+++ 02.Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:How are you Ramesh"
+echo "Ramesh: I am doing good,thanks for asking"
+echo "Suresh: What are you learning, Ramesh"
+echo "Ramesh: I am learning shell scripting"
