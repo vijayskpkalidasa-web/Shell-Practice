c90ce806b7d09fe4bf1fdfc7bf788bdfb5fbe581
VijayKalidasa <VijayKalidasa>
2026-05-01 23:08:17 +0530

    new changes commited

---
+++ 04.Arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VAR1=$1
+VAR2=$2
+
+
+echo "$VAR1:How are you $VAR2"
+echo "$VAR2: I am doing good,thanks for asking"
+echo "$VAR1: What are you learning, $VAR2"
+echo "$VAR2: I am learning shell scripting"
